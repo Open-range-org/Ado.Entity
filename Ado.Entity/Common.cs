05bbb85cc21c7f57955139e165ab86d6969ba719
NIHAR-SARKAR <rana.sarkar61@gmail.com>
2022-07-28 21:56:26 +0530

    Connection file init

---
+++ Ado.Entity/Common.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Ado.Entity
+{
+    public class Common
+    {
+
+    }
+}
---
+++ Ado.Entity/Connection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Ado.Entity
+{
+    class Connection
+    {
+        public List<T> GetDataByQuery<T>(string ConnectionString, string queryFormat, string[] param = null)
+        {
+            List<T> dtoList = new List<T>();
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+                param = param ?? new string[] { };
+                var queryString = string.Format(queryFormat, param);
+                SqlCommand objSqlCommand = new SqlCommand(queryString, con);
+                objSqlCommand.CommandType = CommandType.Text;
+
+                try
+                {
+                    using (SqlDataReader reader = objSqlCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            T dto = GetObject<T>(reader);
+                            dtoList.Add(dto);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return dtoList;
+                }
+            }
+            return dtoList;
+        }
+        T GetObject<T>(SqlDataReader reader)
+        {
+            return (T)Activator.CreateInstance(typeof(T), reader);
+        }
+    }
+}
