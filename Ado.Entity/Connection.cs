4f8c2a2a82c09863a74ba2c797a8324f64bce6df
NIHAR-SARKAR <rana.sarkar61@gmail.com>
2022-08-02 21:58:57 +0530

    update for generic class

--- Ado.Entity/Connection.cs
+++ Ado.Entity/Connection.cs
@@ -18,34 +18,177 @@ namespace Ado.Entity
             List<T> dtoList = new List<T>();
             using (SqlConnection con = new SqlConnection(ConnectionString))
             {
-                if (con.State != ConnectionState.Open)
-                    con.Open();
-                param = param ?? new string[] { };
-                var queryString = string.Format(queryFormat, param);
-                SqlCommand objSqlCommand = new SqlCommand(queryString, con);
-                objSqlCommand.CommandType = CommandType.Text;
+
 
                 try
                 {
+                    if (con.State != ConnectionState.Open)
+                        con.Open();
+                    param = param ?? new string[] { };
+                    var queryString = string.Format(queryFormat, param);
+                    SqlCommand objSqlCommand = new SqlCommand(queryString, con);
+                    objSqlCommand.CommandType = CommandType.Text;
                     using (SqlDataReader reader = objSqlCommand.ExecuteReader())
                     {
-                        while (reader.Read())
-                        {
-                            T dto = GetObject<T>(reader);
-                            dtoList.Add(dto);
-                        }
+
+                        //while (reader.Read())
+                        //{
+                        //    T dto = GetObject<T>(reader);
+                        //    dtoList.Add(dto);
+                        //}
+
+                        var dataTable = new DataTable();
+                        dataTable.Load(reader);
+                        dtoList.AddRange(Map<T>(dataTable));
                     }
+
                 }
                 catch (Exception ex)
                 {
                     return dtoList;
                 }
+                finally
+                {
+                    con.Close();
+                }
             }
             return dtoList;
         }
-        T GetObject<T>(SqlDataReader reader)
+        public bool ExecuteByQuery<T>(string queryFormat, string[] param = null)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+
+                try
+                {
+                    param = param ?? new string[] { };
+                    var queryString = string.Format(queryFormat, param);
+                    SqlCommand objSqlCommand = new SqlCommand(queryString, con);
+                    objSqlCommand.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            return true;
+        }
+        private T GetObject<T>(SqlDataReader reader)
         {
             return (T)Activator.CreateInstance(typeof(T), reader);
+        }
+        private List<T> Map<T>(DataTable dataTable)
+        {
+
+            List<T> _objList = new List<T>();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                T _obj = Execute<T>(dataTable.Columns, row);
+                if (_obj != null)
+                {
+                    _objList.Add(_obj);
+                }
+            }
+            return _objList;
+        }
+        private T Execute<T>(DataColumnCollection column, DataRow row)
+        {
+
+            var _object = (T)Activator.CreateInstance(typeof(T));
+            var properties = _object.GetType().GetProperties();
+            foreach (var property in properties)
+            {
+                var index = column.IndexOf(property.Name);
+                if (index != -1)
+                {
+                    if (property.PropertyType.IsClass && !property.PropertyType.IsPrimitive)
+                    {
+                        var val = ConvertObject(property, row[index]);
+                        property.SetValue(_object, val, null);
+                    }
+                    else
+                    {
+                        try
+                        {
+                            property.SetValue(_object, row[index], null);
+                        }
+                        catch
+                        {
+                            property.SetValue(_object, default(T), null);
+                        }
+                    }
+                }
+
+            }
+            return _object;
+        }
+        private object ConvertObject(PropertyInfo prop, object val)
+        {
+            object convertedValue = null;
+            if (!prop.PropertyType.IsClass)
+            {
+                convertedValue = Convert.ChangeType(val, Type.GetType(prop.PropertyType.Name));
+            }
+            else
+            {
+                var propType = prop.PropertyType.Name;
+                switch (propType)
+                {
+                    case nameof(TypeCode.String):
+                        convertedValue = val.ToString();
+                        break;
+                    case nameof(TypeCode.Int32):
+                        int i;
+                        int.TryParse(val.ToString(), out i);
+                        convertedValue = i;
+                        break;
+                    case nameof(TypeCode.Boolean):
+                        bool b;
+                        Boolean.TryParse(val.ToString(), out b);
+                        convertedValue = b;
+                        break;
+                    case nameof(TypeCode.DateTime):
+                        DateTime d;
+                        DateTime.TryParse(val.ToString(), out d);
+                        convertedValue = d;
+                        break;
+                    case nameof(System.Guid):
+                        convertedValue = new Guid(val.ToString());
+                        break;
+                    case nameof(System.Type):
+                        convertedValue = Type.GetType(val.ToString());
+                        break;
+
+                    default:
+                        convertedValue = val.ToString();
+                        break;
+                }
+            }
+            return convertedValue;
+        }
+        private object LoadRecord(Type valType, object val)
+        {
+            var Instance = Activator.CreateInstance(valType);
+            var properties = Instance.GetType().GetProperties();
+
+            foreach (var property in properties)
+            {
+                if (property.PropertyType.IsClass && !property.PropertyType.IsPrimitive)
+                {
+                    property.SetValue(Instance, LoadRecord(property.PropertyType, val), null);
+                }
+                else
+                {
+                    property.SetValue(Instance, val, null);
+                }
+            }
+            return Instance;
         }
     }
 }
