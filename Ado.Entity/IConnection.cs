e86231bfaaed87d8da68e1257da2424db43ad7fb
NIHAR-SARKAR <rana.sarkar61@gmail.com>
2022-08-03 17:26:06 +0530

    Adding interface Iconnection

---
+++ Ado.Entity/IConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ado.Entity
+{
+    interface IConnection
+    {
+        List<T> GetDataByQuery<T>(string queryFormat, string[] param = null);
+        List<T> GetDataByQuery<T>(string queryFormat);
+        bool AddEntry<T>(T obj);
+        bool UpdateEntry<T>(T obj);
+        bool AddEntry<T>(List<T> objList);
+        bool UpdateEntry<T>(List<T> objList);
+    }
+}
