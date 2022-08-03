237fd85941284f9a14cdc103f5d47c40ae7f90bb
NIHAR-SARKAR <rana.sarkar61@gmail.com>
2022-08-03 17:22:11 +0530

    Custom attribute for entity

---
+++ Ado.Entity/CustomAttributes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ado.Entity
+{
+    public class Primary : Attribute
+    {
+    }
+    public class Unique : Attribute
+    {
+    }
+}
