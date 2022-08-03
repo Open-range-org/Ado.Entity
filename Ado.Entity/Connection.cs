1419b3651468c8efcf666884ba95ec90323b239a
NIHAR-SARKAR <rana.sarkar61@gmail.com>
2022-08-04 03:34:51 +0530

    Code Update

--- Ado.Entity/Connection.cs
+++ Ado.Entity/Connection.cs
@@ -8,12 +8,19 @@ using System.Data.SqlClient;
 
 namespace Ado.Entity
 {
-    class Connection:IConnection
+    public class Connection:IConnection
     {
         private string ConnectionString;
         public Connection(string connectionString)
         {
             ConnectionString = connectionString;
+        }
+        private void Validation<T>()
+        {
+            if(typeof(T).BaseType != typeof(AdoBase))
+            {
+                throw new NotImplementedException("AdoBase class is not inharited in Entity");
+            }
         }
         public List<T> GetDataByQuery<T>(string queryFormat)
         {
@@ -21,6 +28,7 @@ namespace Ado.Entity
         }
         public List<T> GetDataByQuery<T>(string queryFormat, string[] param = null)
         {
+            Validation<T>();
             List<T> dtoList = new List<T>();
             using (SqlConnection con = new SqlConnection(ConnectionString))
             {
@@ -50,8 +58,25 @@ namespace Ado.Entity
             }
             return dtoList;
         }
+        public bool AddEntry<T>(List<T> objList)
+        {
+            try
+            {
+                foreach (T obj in objList)
+                {
+                    AddEntry<T>(obj);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return true;
+        }
         public bool AddEntry<T>(T obj)
         {
+            Validation<T>();
             string queryString = BuildInsertQueryString<T>(obj);
             using (SqlConnection con = new SqlConnection(ConnectionString))
             {
@@ -74,7 +99,80 @@ namespace Ado.Entity
             }
             return true;
         }
+        public bool UpdateEntry<T>(List<T> objList)
+        {
+            try
+            {
+                foreach (T obj in objList)
+                {
+                    UpdateEntry<T>(obj);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
 
+            return true;
+        }
+        public bool UpdateEntry<T>(T obj)
+        {
+            Validation<T>();
+            string queryString = BuildUpdateQueryString<T>(obj);
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+
+                try
+                {
+                    SqlCommand objSqlCommand = new SqlCommand(queryString, con);
+                    objSqlCommand.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            return true;
+        }
+        private string BuildUpdateQueryString<T>(T obj)
+        {
+
+            var primaryKey = typeof(T).GetProperties().Where(a => a.GetCustomAttributes(true).Where(s => s.GetType() == typeof(Primary)).Count() == 1).FirstOrDefault();
+            var queryString = $"UPDATE {obj.GetType().Name} SET ";
+            var properties = obj.GetType().GetProperties();
+
+            string filterQuery = string.Empty;
+            foreach (var property in properties)
+            {
+                if (property.Name == primaryKey.Name)
+                {
+                    filterQuery += $"Where {property.Name}={property.GetValue(obj, null)}";
+                }
+                else
+                {
+                    if (property.PropertyType.Name == "String" || property.PropertyType.Name == "Type"|| property.PropertyType.Name == "DateTime")
+                    {
+                        queryString += $"[{property.Name}]='{property.GetValue(obj, null)}',";
+                    }
+                    else if (property.PropertyType.Name == "Boolean")
+                    {
+                        queryString += $"[{property.Name}]={Convert.ToByte(property.GetValue(obj, null))},";
+                    }
+                    else
+                    {
+                        queryString += $"[{property.Name}]={property.GetValue(obj, null)},";
+                    }
+                }
+            }
+            queryString = queryString.Remove(queryString.Length - 1, 1) + $" {filterQuery}";
+            return queryString;
+        }
         private string BuildInsertQueryString<T>(T obj)
         {
 
@@ -87,17 +185,17 @@ namespace Ado.Entity
             queryString = queryString.Remove(queryString.Length - 1, 1) + ") VALUES (";
             foreach (var property in properties)
             {
-                if (property.PropertyType.Name == "String" || property.PropertyType.Name == "Type")
+                if (property.PropertyType.Name == "String" || property.PropertyType.Name == "Type" || property.PropertyType.Name == "DateTime")
                 {
-                    queryString += $"'{property.GetValue(obj)}',";
+                    queryString += $"'{property.GetValue(obj,null)}',";
                 }
                 else if (property.PropertyType.Name == "Boolean")
                 {
-                    queryString += $"{Convert.ToByte(property.GetValue(obj))},";
+                    queryString += $"{Convert.ToByte(property.GetValue(obj, null))},";
                 }
                 else
                 {
-                    queryString += $"{property.GetValue(obj)},";
+                    queryString += $"{property.GetValue(obj, null)},";
                 }
             }
             queryString = queryString.Remove(queryString.Length - 1, 1) + $");SET IDENTITY_INSERT {obj.GetType().Name} OFF ;";
@@ -132,7 +230,7 @@ namespace Ado.Entity
                 var index = column.IndexOf(property.Name);
                 if (index != -1)
                 {
-                    if (property.PropertyType.IsClass && !property.PropertyType.IsPrimitive)
+                    if (property.PropertyType.IsClass || property.PropertyType.IsPrimitive)
                     {
                         var val = ConvertObject(property, row[index]);
                         property.SetValue(_object, val, null);
@@ -149,51 +247,78 @@ namespace Ado.Entity
                         }
                     }
                 }
-
             }
             return _object;
         }
         private object ConvertObject(PropertyInfo prop, object val)
         {
             object convertedValue = null;
-            if (!prop.PropertyType.IsClass)
+            //if (prop.PropertyType.IsClass)
+            //{
+            //    convertedValue = Convert.ChangeType(val, Type.GetType(prop.PropertyType.Name));
+            //}
+            var propType = prop.PropertyType.Name;
+            switch (propType)
             {
-                convertedValue = Convert.ChangeType(val, Type.GetType(prop.PropertyType.Name));
-            }
-            else
-            {
-                var propType = prop.PropertyType.Name;
-                switch (propType)
-                {
-                    case nameof(TypeCode.String):
-                        convertedValue = val.ToString();
-                        break;
-                    case nameof(TypeCode.Int32):
-                        int i;
-                        int.TryParse(val.ToString(), out i);
-                        convertedValue = i;
-                        break;
-                    case nameof(TypeCode.Boolean):
-                        bool b;
-                        Boolean.TryParse(val.ToString(), out b);
-                        convertedValue = b;
-                        break;
-                    case nameof(TypeCode.DateTime):
-                        DateTime d;
-                        DateTime.TryParse(val.ToString(), out d);
-                        convertedValue = d;
-                        break;
-                    case nameof(System.Guid):
-                        convertedValue = new Guid(val.ToString());
-                        break;
-                    case nameof(System.Type):
-                        convertedValue = Type.GetType(val.ToString());
-                        break;
+                case nameof(TypeCode.String):
+                    convertedValue = val.ToString();
+                    break;
+                case nameof(TypeCode.Int16):
+                    short s;
+                    short.TryParse(val.ToString(), out s);
+                    convertedValue = s;
+                    break;
+                case nameof(TypeCode.Int32):
+                    int i;
+                    int.TryParse(val.ToString(), out i);
+                    convertedValue = i;
+                    break;
+                case nameof(TypeCode.Int64):
+                    long l;
+                    long.TryParse(val.ToString(), out l);
+                    convertedValue = l;
+                    break;
+                case nameof(TypeCode.Boolean):
+                    bool b;
+                    Boolean.TryParse(val.ToString(), out b);
+                    convertedValue = b;
+                    break;
+                case nameof(TypeCode.Decimal):
+                    Decimal dc;
+                    Decimal.TryParse(val.ToString(), out dc);
+                    convertedValue = dc;
+                    break;
+                case nameof(TypeCode.Double):
+                    Double db;
+                    Double.TryParse(val.ToString(), out db);
+                    convertedValue = db;
+                    break;
+                case nameof(TypeCode.Byte):
+                    Byte by;
+                    Byte.TryParse(val.ToString(), out by);
+                    convertedValue = by;
+                    break;
+                case nameof(TypeCode.Single):
+                    float f;
+                    float.TryParse(val.ToString(), out f);
+                    convertedValue = f;
+                    break;
 
-                    default:
-                        convertedValue = val.ToString();
-                        break;
-                }
+                case nameof(TypeCode.DateTime):
+                    DateTime d;
+                    DateTime.TryParse(val.ToString(), out d);
+                    convertedValue = d;
+                    break;
+                case nameof(System.Guid):
+                    convertedValue = new Guid(val.ToString());
+                    break;
+                case nameof(System.Type):
+                    convertedValue = Type.GetType(val.ToString());
+                    break;
+
+                default:
+                    convertedValue = val.ToString();
+                    break;
             }
             return convertedValue;
         }
