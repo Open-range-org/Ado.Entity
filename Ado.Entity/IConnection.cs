b886f1cc9c7a75bf8829ff105b7752ea289859d3
NIHAR-SARKAR <rana.sarkar61@gmail.com>
2022-12-23 14:14:19 +0530

    SqlDelete - Adding new delete feature

--- Ado.Entity/IConnection.cs
+++ Ado.Entity/IConnection.cs
@@ -12,5 +12,7 @@ namespace Ado.Entity
         bool UpdateEntry<T>(T obj);
         bool AddEntry<T>(List<T> objList);
         bool UpdateEntry<T>(List<T> objList);
+        bool DeleteEntry<T>(List<T> objList);
+        bool DeleteEntry<T>(T obj);
     }
 }
